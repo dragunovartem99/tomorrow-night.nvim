a9b2580e43651ac6207e2428cae0fe2f9a8f855b
Artem Dragunov <dragunovartem99@gmail.com>
2026-02-15 21:30:45 +0300

    Init

---
+++ lua/tomorrow-night/palette.lua
@@ -0,0 +1,25 @@
+-- Tomorrow Night by Chris Kempson (http://chriskempson.com)
+
+-- https://github.com/chriskempson/vim-tomorrow-theme/blob/master/colors/Tomorrow-Night.vim
+-- https://github.com/Gogh-Co/Gogh/blob/master/themes/Tomorrow%20Night.yml
+
+-- stylua: ignore
+
+local palette = {
+	black        = "#282A2E",
+	red          = "#CC6666",
+	green        = "#B5BD68",
+	yellow       = "#F0C674",
+	blue         = "#81A2BE",
+	magenta      = "#B294BB",
+	cyan         = "#8ABEB7",
+	white        = "#C5C8C6",
+
+	bright_black = "#969896",
+	bright_white = "#FFFFFF",
+
+	background   = "#1D1F21",
+	foreground   = "#C5C8C6",
+}
+
+return palette
