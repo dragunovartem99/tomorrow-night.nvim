909bac6f97b0eff4b15a7dec5d9f5efa19b51930
Artem Dragunov <dragunovartem99@gmail.com>
2026-02-20 01:41:49 +0300

    Add more links for treesitter

--- lua/programming/treesitter.lua
+++ lua/programming/treesitter.lua
@@ -3,5 +3,6 @@ local abstract = require("abstract")
 return {
 	["@constructor"] = abstract("CLASS"),
 	["@tag.attribute"] = abstract("PROPERTY"),
+	["@variable.member"] = abstract("PROPERTY"),
 	["@type.builtin"] = abstract("TYPE"),
 }
